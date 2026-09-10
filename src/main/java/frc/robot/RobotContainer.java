43d5a3804e73213589eba51fefd4d02d7c2f405a
Lupeydev <197533886+Lupeydev@users.noreply.github.com>
2026-09-10 14:33:56 -0500

    removed pivotsubsytem.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -36,7 +36,6 @@ import frc.robot.motor_ctl.MotorController;
 import frc.robot.motor_ctl.TalonFXSMotorController;
 // Constants
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.PivotSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 // import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.Vision;
